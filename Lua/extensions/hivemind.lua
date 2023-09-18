979a235c3a5a6f3f2acb4aabbfe386d254814700
Mr. 27 <45323883+27alaing@users.noreply.github.com>
2023-09-18 17:12:56 -0400

    added some checks

--- Lua/extensions/hivemind.lua
+++ Lua/extensions/hivemind.lua
@@ -6,7 +6,7 @@ extension.Init = function ()
     Hook.Add("chatMessage", "Traitormod.ChatMessageHivemindComms", function(message, client)
         local character = client.Character
 
-        if character and character.IsHusk then
+        if character and not character.IsHuman and character.SpeciesName ~= "Crawler" then
             for _, loopclient in pairs(Client.ClientList) do
                 if (not loopclient.Character or loopclient.Character.IsDead) or not loopclient.Character.IsHuman then
                     local formatedname = string.format(Traitormod.Language.CMDHuskChat, client.Name, client.Character.Name)
