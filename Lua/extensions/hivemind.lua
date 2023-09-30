ddf9b881f42e43f20be9ca37c101c291d96eaa31
Mr. 27 <45323883+27alaing@users.noreply.github.com>
2023-09-30 18:51:47 -0400

    remobr

--- Lua/extensions/hivemind.lua
+++ Lua/extensions/hivemind.lua
@@ -6,8 +6,6 @@ extension.Init = function ()
     Hook.Add("chatMessage", "Traitormod.ChatMessageHivemindComms", function(message, client)
         local character = client.Character
 
-        if message:sub(1, 1) == "!" then return true end
-
         if character and not character.IsHuman and character.SpeciesName ~= "Crawler" then
             Traitormod.SendHuskChatMessage(message, client)
             return true
